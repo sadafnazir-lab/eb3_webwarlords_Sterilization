fa25912255db529ffd3112a6f33d477bbbd8057f
sadafnazir-lab <69804253+sadafnazir-lab@users.noreply.github.com>
2020-09-02 13:51:15 +0530

    Create pretest.js

---
+++ Experiment/pretest.js
@@ -0,0 +1,31 @@
+# **4.Pre Test**
+
+1. The temperature that allows for most rapid growth during a short period of time is known as \_\_\_\_\_\_\_\_\_\_
+a) Minimum Temperature
+b) Maximum Temperature
+**c) Optimum Temperature**
+d) Growth Temperature
+
+2. Mesophiles are group of bacteria that grow within the temperature range of?
+a) 0-20 degree Celsius
+**b) 25-40 degree Celsius**
+c) 45-60 degree Celsius
+d) more than 60 degree Celsius
+
+3. The optimum pH for the growth of most bacteria lies between \_\_\_\_\_\_\_
+a) 5-9
+**b) 6.5-7.5**
+c) 2-3.5
+d) 9-9.5
+
+4. Which of the following is used as a solidifying agent for media?
+a) Beef extract
+b) Peptone
+**c) Agar**
+d) Yeast extract
+
+6. Which of the following instrument is used for sterilizing the media after it has been prepared?
+**a) Autoclave**
+b) Laminar Air Flow Chamber
+c) Inoculum Needle
+d) Incubator
